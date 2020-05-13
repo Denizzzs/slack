718b8cf0b1c1ce485e2e51c5da7e05c1df1eda69
“Asiya59” <sistedeniz.1920@gmail.com>
2020-05-13 16:59:10 +0200

    update createchannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,13 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println(" I create channel");
+        int x=0;
+        if (x==0){
+            System.out.println("equal");
+        }
+        int x=0;
+        if (x==0){
+            System.out.println("equal");
+        }
     }
 }
--- src/loginClass.java
+++ src/loginClass.java
@@ -4,5 +4,11 @@ public class loginClass {
         
         
         // brand new changes from github
+        String s= "a";
+        if(s.contains("a")){
+            System.out.println("contains");
+        }else{
+            System.out.println("not");
+        }
     }
 }
